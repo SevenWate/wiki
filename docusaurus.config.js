c8b04da65567e5db978f2c65e586a366784d7efa
7Wate <zhouzhongping@7wate.com>
2022-07-27 09:04:08 +0800

    项目：更新网站配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -282,19 +282,6 @@ const config = {
         defaultLanguage: "markdown",
         additionalLanguages: ["java", "git"],
       },
-      // imageZoom: {
-      //   // CSS selector to apply the plugin to, defaults to '.markdown img'
-      //   selector: ".markdown img",
-      //   // Optional medium-zoom options
-      //   // see: https://www.npmjs.com/package/medium-zoom#options
-      //   options: {
-      //     margin: 24,
-      //     background: "#BADA55",
-      //     scrollOffset: 0,
-      //     container: "#zoom-container",
-      //     template: "#zoom-template",
-      //   },
-      // },
     }),
 };
 
